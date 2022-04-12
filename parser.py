5ec34b24e566b89b61b6effb929c41abd70a8022
WeWe-power <dmant.rodionov@gmail.com>
2022-04-12 15:55:41 +0300

    Full project commit

---
+++ parser.py
@@ -0,0 +1,72 @@
+import requests
+import json
+import time
+from bs4 import BeautifulSoup, SoupStrainer
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15",
+    "Accept-Language": "ru",
+    "Connection": "keep-alive",
+}
+parser = 'lxml'
+
+
+def get_data(url: str) -> str | None:
+    """
+    Returns string containing all html code from a page with given url
+    """
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        return None
+    return response.text
+
+
+def extract_product_info(soup: BeautifulSoup) -> dict:
+    """
+    Extracts all data about product and saves it to dict
+    """
+    product_price = ''.join(soup.find('del', class_='price-block__old-price').text.strip().split()[:-1])
+    product_price_with_sale = ''.join(soup.find('span', class_='price-block__final-price').text.strip().split()[:-1])
+
+    brand_and_name = soup.find('h1', class_='same-part-kt__header').find_all('span')
+    product_brand = brand_and_name[0].text.strip()
+    product_name = brand_and_name[1].text.strip()
+
+    product_article = soup.find('span', id='productNmId').text.strip()
+
+    product_detail = {
+        'product_brand': product_brand,
+        'product_name': product_name,
+        'product_article': product_article,
+        'product_price': product_price,
+        'product_price_with_sale': product_price_with_sale,
+    }
+
+    return product_detail
+
+
+def get_product_info(url: str) -> dict | None:
+    html = get_data(url)
+
+    # check if data is get properly
+    if html:
+        # All info that we need is contained in div with class product-detail so lets parse only from that div
+        # Make parsing faster for about 40%
+        div_containing_product_details = SoupStrainer(class_="product-detail")
+        soup = BeautifulSoup(html, parser, parse_only=div_containing_product_details)
+
+        # getting product details
+        product_detail = extract_product_info(soup)
+
+        # writing data to output file
+        with open('products.json', 'w') as file:
+            json.dump(product_detail, file, indent=4, ensure_ascii=False)
+        return product_detail
+    else:
+        return None
+
+
+url = 'https://www.wildberries.ru/catalog/70796973/detail.aspx?targetUrl=GP'
+get_product_info(url)
