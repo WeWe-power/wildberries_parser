f75d127627a5eba876a404c242105b3762cb54ad
WeWe-power <dmant.rodionov@gmail.com>
2022-04-16 03:39:00 +0300

    Added waiting for dynamic content to be loaded

--- parser.py
+++ parser.py
@@ -1,14 +1,35 @@
-import json
-
+import time
 from bs4 import BeautifulSoup, SoupStrainer
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException, InvalidArgumentException
 
 parser = 'lxml'
+
+provider_element_classes_and_tags= {
+    'span': 'price-block__final-price',
+    'del': 'price-block__old-price',
+}
+
+
+def get_web_driver() -> webdriver.Chrome:
+    """
+    Getting selenium webdriver that make background browser operations
+    """
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
+    capabilities = options.to_capabilities()
+    web_driver = webdriver.Remote(
+        command_executor='http://hub:4444/wd/hub',
+        desired_capabilities=capabilities,
+    )
+    return web_driver
 
 
 def get_soup_parser(
@@ -27,22 +48,12 @@ def get_soup_parser(
     return soup
 
 
-def get_web_driver() -> webdriver.Chrome:
-    """
-    Getting selenium webdriver that make hidden browser operations and has hidden webdriver flag
-    """
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    web_driver = webdriver.Chrome(options=options)
-
-    return web_driver
-
-
-def get_data(url: str) -> str | None:
+def get_data(url: str) -> tuple | None:
     """
     Gets html page including dynamically loaded by js content
     """
+
+    # Get webdriver
     driver = get_web_driver()
 
     # Check if url is correct
@@ -52,15 +63,19 @@ def get_data(url: str) -> str | None:
         return None
 
     # Tries to find producer info or returns None if cannot find( it may be caused by wrong link )
-    try:
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "seller-details__title")))
-    except TimeoutException:
-        return None
+    provider_elem_class = wait_for_elems(driver, provider_element_classes_and_tags)
 
-    return driver.page_source
+    # Getting html of page and destroying driver session
+    html = driver.page_source
+    driver.quit()
+
+    return html, provider_elem_class
 
 
-def extract_product_info(html: str) -> dict[str, str]:
+def extract_product_info(
+        html: str,
+        provider_elem_class_and_tag: dict
+) -> dict[str, str]:
     """
     Extracts all info about product and return dict containing product info,
     exactly: price, sale price, brand, name, vendor  code
@@ -81,18 +96,41 @@ def extract_product_info(html: str) -> dict[str, str]:
     product_name = brand_and_name[1].text.strip()
 
     product_vendor_code = soup.find('span', id='productNmId').text.strip()
-    provider = soup.find('a', class_='seller-details__title').text.strip()
+    tag, element_class = next(iter(provider_elem_class_and_tag.items()))
+    provider = soup.find(tag, class_=element_class).text.strip()
 
     product_detail = {
-        'product_brand': product_brand,
-        'product_name': product_name,
-        'product_vendor_code': product_vendor_code,
-        'product_price': product_price,
-        'product_price_with_sale': product_price_with_sale,
+        'brand': product_brand,
+        'name': product_name,
+        'vendor_code': product_vendor_code,
+        'price': product_price,
+        'price_with_sale': product_price_with_sale,
         'provider': provider,
     }
 
     return product_detail
+
+
+def wait_for_elems(
+        driver: webdriver,
+        elems_classes_with_tags_list: dict,
+) -> bool | dict:
+    """
+    Functions that waits for one of the elements from list to be found on the page,
+    if nothing found in 5 seconds returns false
+    """
+    start_time = time.time()
+    time_now = time.time()
+    while time_now - start_time < 5:
+        for elem_tag, elem_class in elems_classes_with_tags_list.items():
+            try:
+                driver.find_element(By.CLASS_NAME, elem_class)
+                return {elem_tag: elem_class}
+            except Exception as ex_:
+                pass
+        time.sleep(0.05)
+        time_now = time.time()
+    return False
 
 
 def get_product_info(url: str) -> dict[str, str] | None:
@@ -100,14 +138,15 @@ def get_product_info(url: str) -> dict[str, str] | None:
     Parse wildberries product info by product url,
 
     """
-    html = get_data(url)
+
+    # place vendor code into our url pattern
+    html, provider_elem_class_and_tag = get_data(url)
+
     if html:
         # getting product details
-        product_detail = extract_product_info(html)
+        product_detail = extract_product_info(html, provider_elem_class_and_tag)
 
         # writing data to output file
-        with open('products.json', 'w') as file:
-            json.dump(product_detail, file, indent=4, ensure_ascii=False)
         return product_detail
     else:
         return None
